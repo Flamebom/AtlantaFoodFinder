7621664aadcfe249ffa7aa63421738e8af4ddfc9
Flamebom <kylechess8@gmail.com>
2024-09-29 16:23:38 -0400

    Added google request function for by cuisine and name

--- AtlantaFoodFinder/GoogleAPI/GoogleRequest.py
+++ AtlantaFoodFinder/GoogleAPI/GoogleRequest.py
@@ -6,9 +6,32 @@ Google request class allows for the get_google_restaurants to return a list of t
 It scans nearby for a 500 meter radius for nearby things and returns it in the list.
 """
 __URL = "https://places.googleapis.com/v1/places:searchNearby"
-__API_KEY = 'AIzaSyAn2GEz9YyAxynT77ezXbhIF9Ua9iy2ABk'
-
-
+__API_KEY = ''
+__URLNAME = "https://places.googleapis.com/v1/places:searchText"
+def get_google_restaurants_name(name_or_cuisine):
+    payload = json.dumps({
+        "textQuery": name_or_cuisine,
+        "maxResultCount": 10,
+        "locationBias": {
+            "rectangle": {
+                "low": {
+                    "latitude": 33.50457675495219,
+                    "longitude": -84.63554645925697
+                },
+                "high": {
+                    "latitude": 34.217961053179856,
+                    "longitude": -83.76726126632337
+                }
+            }
+        }
+    })
+    headers = {
+        'X-Goog-Api-Key': __API_KEY,
+        'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.primaryType,places.location,places.rating,places.nationalPhoneNumber,places.websiteUri,places.reviews',
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", __URLNAME, headers=headers, data=payload)
+    return format_results(response)
 def get_google_restaurants(latitude, longitude):
     payload = json.dumps({
         "includedTypes": [
@@ -31,6 +54,8 @@ def get_google_restaurants(latitude, longitude):
         'Content-Type': 'application/json'
     }
     response = requests.request("POST", __URL, headers=headers, data=payload)
+    return format_results(response)
+def format_results(response):
     rawdata = json.loads(response.text)
     list_restaurants =  (rawdata["places"])
     restaurant_output = []
--- AtlantaFoodFinder/GoogleAPI/GoogleRequestExamples.py
+++ AtlantaFoodFinder/GoogleAPI/GoogleRequestExamples.py
@@ -1,13 +1,15 @@
 import GoogleRequest
 #gets restaurants
-outputlist = GoogleRequest.get_google_restaurants(45.55860601168696, -122.86519658540723)
+#outputlist = GoogleRequest.get_google_restaurants(45.55860601168696, -122.86519658540723)
 #Allows for the print of any attribute, ordered in a list.
 #all information in string format of second restaurant
-print("all information in string format of third restaurant")
-print(outputlist[2])
+#print("all information in string format of third restaurant")
+#print(outputlist[2])
 #name of first restaurant
-print("name of first restaurant")
-print(outputlist[0].name)
+#print("name of first restaurant")
+#print(outputlist[0].name)
 #rating of fourth restaurant
-print("rating of fourth restaurant")
-print(outputlist[3].rating)
+#print("rating of fourth restaurant")
+#print(outputlist[3].rating)
+outputlist2 = GoogleRequest.get_google_restaurants_name("dim sum")
+print(outputlist2[0])
