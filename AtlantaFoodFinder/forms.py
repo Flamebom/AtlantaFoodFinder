b707e96860ac9f9e503b3e4efcc40a2345807c45
ARKNAV <arnavketineni@gmail.com>
2024-09-30 16:01:11 -0400

    done part A and also part E

--- AtlantaFoodFinder/forms.py
+++ AtlantaFoodFinder/forms.py
@@ -4,4 +4,4 @@ from .models import CustomUser  # If you are using a custom user model, otherwis
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['email_address', 'password1', 'confirmation_password']
+        fields = ['username', 'password1', 'confirmation_password']
