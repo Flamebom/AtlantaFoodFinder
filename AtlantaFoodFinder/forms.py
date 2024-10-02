bb47b93a88b65d20f26b18e2c5c8a7d2ebf58d60
ARKNAV <122695841+ARKNAV@users.noreply.github.com>
2024-10-02 08:18:59 -0400

    Update forms.py

--- AtlantaFoodFinder/forms.py
+++ AtlantaFoodFinder/forms.py
@@ -5,10 +5,10 @@ from .models import CustomUser  # Import your custom user model
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['email_address', 'password1', 'password2']  # Use your custom fields
+        fields = ['email', 'password1']  # Use your custom fields
 
     def clean_email_address(self):
-        email = self.cleaned_data.get('email_address')
+        email = self.cleaned_data.get('email')
         if CustomUser.objects.filter(email_address=email).exists():
             raise forms.ValidationError("Email already exists.")
         return email
