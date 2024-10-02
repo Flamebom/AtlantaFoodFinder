6c872f25c601e0f736d8528ba1eb1e59cf862afe
ARKNAV <122695841+ARKNAV@users.noreply.github.com>
2024-10-02 09:08:04 -0400

    Update urls.py to properly configure reset password

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -26,20 +26,15 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout.html/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('passwordreset/', views.ResetPasswordView.as_view(), name='password_reset'),
-    path('passwordreset.html/', views.ResetPasswordView.as_view(), name='password_reset'),
+    path('password_reset/', views.ResetPasswordView.as_view(), name='password_reset'),
+    path('password_reset.html/', views.ResetPasswordView.as_view(), name='password_reset'),
 
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('password-reset-confirm.html/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
-         name='password_reset_complete'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='login.html'),
+         name='password_reset_done'),  # Adjust template name if needed
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='login.html'),
+         name='password_reset_confirm'),  # Confirm reset link
     path('password-reset-complete.html/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         auth_views.PasswordResetCompleteView.as_view(template_name='login.html'),
          name='password_reset_complete'),
 ]
